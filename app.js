8318dff228c2bcdc0d1e4a32ae76cfc1b5b5d4ce
gauntc <gaunt.caleb@gmail.com>
2017-10-18 00:22:42 -0600

    Got the overall functionality working

--- app.js
+++ app.js
@@ -1,56 +1,78 @@
-angular.module('News', ['ui.router'])
+angular.module('MockFeed', ['ui.router'])
   .config([ '$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
     $stateProvider.state('home',
     {
       url: '/home',
       templateUrl: '/home.html',
-      controller: 'MainCtrl'
-    }).state('posts',
+      controller: 'HomeCtrl'
+    }).state('quiz',
     {
       url: '/quiz/{src}',
       templateUrl: '/quiz.html',
       controller: 'QuizCtrl'
+    }).state('result',
+    {
+      url: '/result',
+      templateUrl: '/result.html',
+      controller: 'ResultCtrl'
     });
     $urlRouterProvider.otherwise('home');
   }])
-  //From here on out it's from the old assignment
-  .factory('postFactory', [
-    function(){
-      var o = { posts: [] };
-      return o;
-  }])
-  .controller('MainCtrl', [ '$scope', '$http', function($scope, $http){
+  .service('quizService', function () {
+    return {
+      quiz : {},
+      answers : []
+    };
+  })
+  .controller('HomeCtrl', [ '$scope', '$http', 'quizService', function($scope, $http, quizService) {
     $scope.quizzes = [];
     $http.get('manifest.json').then(function(data) {
       console.log('data', data);
       data = data.data;
-      var quizList = data.quizLink;
-      for( var i = 0; i < quizList.length; i++) {
-       $scope.quizzes.push(quizList[i]);
+      var quizList = data.quizLinks;
+      for (var i = 0; i < quizList.length; i++) {
+        $scope.quizzes.push(quizList[i]);
       }
     });
-
-    /*$scope.test = 'Hello world!';
-    $scope.posts = postFactory.posts;
-    $scope.addPost = function(){
-      if($scope.formContent === '') { return; }
-      $scope.posts.push({ title: $scope.formContent, upvotes: 0, comments: [ ] });
-      $scope.formContent = '';
-    };
-    $scope.incrementUpvotes = function(post) {
-      post.upvotes += 1;
-    };*/
   }])
-  .controller('PostsCtrl', [ '$scope', '$stateParams', 'postFactory', function($scope, $stateParams, postFactory){
-    $scope.post = postFactory.posts[$stateParams.id];
-    $scope.addComment = function(){
-      if($scope.body === '') {
-        return;
+  .controller('QuizCtrl', [ '$scope', '$stateParams', '$http', '$location', 'quizService', function($scope, $stateParams, $http, $location, quizService) {
+    $http.get($stateParams.src).then(function(data) {
+      console.log('data', data);
+      data = data.data;
+      quizService.quiz = data;
+      quizService.answers = new Array(data.questions[0].answers.length);
+      for (var i = 0; i < quizService.answers.length; i++) {
+        quizService.answers[i] = 0;
       }
-      $scope.post.comments.push({ body: $scope.body, upvotes: 0 });
-      $scope.body = '';
-    };
-    $scope.incrementUpvotes = function(comment){
-      comment.upvotes += 1;
-    };
+
+      $scope.quizTitle = quizService.quiz.title;
+      $scope.questions = quizService.quiz.questions;
+    });
+
+    $scope.currQuestionIndex = 0;
+    $scope.submitAnswer = function(option) {
+      var optionIndex = $scope.questions[$scope.currQuestionIndex].answers.indexOf(option);
+      $scope.currQuestionIndex++;
+      quizService.answers[optionIndex]++;
+      if ($scope.currQuestionIndex == $scope.questions.length) {
+        $location.path('/result');
+      }
+    }
+  }])
+  .controller('ResultCtrl', [ '$scope', '$http', '$location', 'quizService', function($scope, $http, $location, quizService) {
+    $scope.result = quizService.quiz.results[0].resultText;
+    $scope.answers = quizService.answers;
+    $scope.numberOfQuestions = quizService.quiz.questions.length;
+    $scope.getProgressBarLabel = function(index, amount) {
+      var percentage = 100 * amount / $scope.numberOfQuestions;
+      if (percentage === 0) {
+        percentage = "0";
+      }
+      return String.fromCharCode(65 + index) + " : " + percentage + "%";
+    }
+    $scope.returnToHome = function() {
+      $location.path('/home');
+      quizService.quiz = {};
+      quizService.answers = [];
+    }
   }]);
